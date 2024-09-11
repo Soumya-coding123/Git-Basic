4fcacb92ea45d2ede283ea341a83aa0977691afe
Soumya <soumyasataiah@gmail.com>
2024-09-11 22:02:03 +0530

    Added Navbar

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function NavBar(){
+    return(
+        <>
+          <ul>
+            <li></li>
+            <li>Home</li>
+            <li>Home</li>
+            <li>Home</li>
+          </ul>
+        </>
+    )
+}
+export default NavBar;
